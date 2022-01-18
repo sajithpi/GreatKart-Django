ea8863e3e3db648bd4acb0d9d25f9dc68989e2d5
SAJITH P I <sajithpalamanna@gmail.com>
2022-01-18 14:00:19 +0530

    Added Ordering and Payment Feature

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -144,8 +144,8 @@ MESSAGE_TAGS = {
 #SMTP configuration
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'radioboxmail@gmail.com'
-EMAIL_HOST_PASSWORD = 'radioboxsaji@15587//@@9$'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 
